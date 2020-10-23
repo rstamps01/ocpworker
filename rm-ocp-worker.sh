0ac249f1b13e2443372dcd1c7c31b7440bddcf21
rstamps01 <67928978+rstamps01@users.noreply.github.com>
2020-10-22 17:07:27 -0700

    Update rm-ocp-worker.sh

--- rm-ocp-worker.sh
+++ rm-ocp-worker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 rm /etc/multipath.conf
-echo "removed /etc/multipath.conf"
+echo "1. Removed /etc/multipath.conf"
 rm /etc/dbus-1/system.d/org.ganesha.nfsd.conf
 systemctl restart messagebus
-echo "removed /etc/dbus-1/system.d/org.ganesha.nfsd.conf"
+echo "2. Removed /etc/dbus-1/system.d/org.ganesha.nfsd.conf"
 systemctl stop metacache
 systemctl disable metacache
 rm /etc/systemd/system/metacache.service
-echo "stopped, disabled, and removed /etc/systemd/system/metacache.service"
+echo "3. Stopped, disabled, and removed /etc/systemd/system/metacache.service"
